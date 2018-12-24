57ae9421fee636d1e723c2254e2251cd38e1e318
ChuJunfang <yaya@CNyaya.local>
2018-12-24 13:23:34 +0800

    hh

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   presets: [
-    '@vue/app'
+    '@vue/app',
+    // ["es2015", {
+    //   "modules": false
+    // }]
+  ],
+  plugins: [
+    [
+      "component",
+      {
+        "libraryName": "element-ui",
+        "styleLibraryName": "theme-chalk"
+      }
+    ]
   ]
 }
